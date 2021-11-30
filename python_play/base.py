7c033fadc9150251446a440709f91378effbd955
Responsibleuk <Responsibleuk@users.noreply.github.com>
2021-11-30 16:55:02 +0000

    ✅ Ready to clone and code.

--- python_play/base.py
+++ python_play/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+python_play base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the python_play project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
